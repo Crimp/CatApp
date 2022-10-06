c82700e9cb67d0f6e84060c23bf98d2cb2489f67
Crimp <dmitry.minakov@devexpress.com>
2022-10-06 15:05:50 +0400

    .

--- CatApp/Program.cs
+++ CatApp/Program.cs
@@ -1,19 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using CatApp;
-using CatCore;
-using CatShelter;
+﻿using CatCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-
+//You cannot change this
 IServiceCollection services = new ServiceCollection();
 services.AddCatCoreServices();
-services.AddCatShelterServices();
 
-services.AddScoped<CatCustomizer>();
-
-var s = services.BuildServiceProvider();
-using(var scope = s.CreateScope()) {
-    var cat = scope.ServiceProvider.GetRequiredService<ICatFactory>().CreateCat();
-    Console.WriteLine(cat);
+using(var scope = services.BuildServiceProvider().CreateScope()) {
+    Console.WriteLine(scope.ServiceProvider.GetRequiredService<ICatFactory>().CreateCat());
 }
---
+++ CatApp/StartupExtensions.cs
@@ -0,0 +1,13 @@
+﻿using CatApp;
+
+namespace Microsoft.Extensions.DependencyInjection;
+public static class StartupExtensions {
+    public static IServiceCollection AddCatServices(this IServiceCollection services) {
+        services.AddCatCoreServices();
+        services.AddCatShelterServices();
+
+        services.AddScoped<CatCustomizer>();
+        return services;
+    }
+}
+
